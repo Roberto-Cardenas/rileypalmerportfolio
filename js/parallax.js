692c34f799633035f78a7e765fa0bcb75c0d6a6e
Roberto Cardenas <robertocardenas@berkeley.edu>
2022-12-26 00:15:13 -0800

    Made parallax more subtle, added links to the hero calls to action

--- js/parallax.js
+++ js/parallax.js
@@ -2,5 +2,5 @@ const parallaxTargetHero = document.querySelector('.hero__image');
 
 window.addEventListener('scroll', () => {
   const wScroll = window.scrollY;
-  parallaxTargetHero.style.objectPosition = "right " + wScroll * 0.2 + "px";
+  parallaxTargetHero.style.objectPosition = "right " + wScroll * 0.07 + "px";
 });
