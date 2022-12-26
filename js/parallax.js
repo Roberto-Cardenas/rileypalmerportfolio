77f2ce990ca24e684feaa956116c369ae7a28b16
Roberto Cardenas <robertocardenas@berkeley.edu>
2022-12-26 15:10:55 -0800

    Added about section

--- js/parallax.js
+++ js/parallax.js
@@ -1,5 +1,6 @@
 const parallaxTargetHero = document.querySelector('.hero__image');
 
+
 window.addEventListener('scroll', () => {
   const wScroll = window.scrollY;
   parallaxTargetHero.style.objectPosition = "right " + wScroll * 0.07 + "px";
